945d60b0a385059bb3ba69e5406551f1e526864f
Coolest101Group <no.root.toe@gmail.com>
2016-07-04 15:41:04 -0700

    finished challenge 1 in python

--- challenge-1/anagram.py
+++ challenge-1/anagram.py
@@ -1,3 +1,22 @@
 """Anagrams"""
 
 
+def isAnagram(word1, word2):
+  list1 = list(word1)
+  list2 = list(word2)
+  list1.sort()
+  list2.sort()
+  if(list1 == list2):
+    return 1
+  else:
+    return 0
+
+
+firstWords = ["cinema", "host", "aba", "train"]
+secondWords = ["iceman", "host", "bab", "rain"]
+
+def check_anagrams(firstWords, secondwords):
+  for i in range(len(firstWords)):
+    print isAnagram(firstWords[i], secondWords[i])
+
+check_anagrams(firstWords, secondWords)
